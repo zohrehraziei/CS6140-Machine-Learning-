ec53f9f972647437d21abe44475892917b3d58ad
Zohreh Raziei <31635558+zohrehraziei@users.noreply.github.com>
2020-02-04 13:12:02 -0500

    Update Gumbel distribution.py

--- src/Gumbel distribution.py
+++ src/Gumbel distribution.py
@@ -1,14 +1,6 @@
-
-"""
-Created on Fri Jan 31 2020
-
-Gumbel Distribution
-
-Author: Zohreh Raziei - raziei.z@husky.neu.edu
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def partial_derivatives(X, beta, alpha):
@@ -52,7 +44,7 @@ def visualization(data, beta , alpha, dis):
     b = (np.std(data) * np.sqrt(6)) / np.pi
     a = np.mean(data) - (0.57721 * beta)
 
-    #getting parameters with help of parameters estimations
+    # getting parameters with help of parameters estimations
     b_prediction, a_prediction = estimating_parameters(data, max_iters, gema) #
 
     # visulization
@@ -72,27 +64,35 @@ def visualization(data, beta , alpha, dis):
     plt.xlabel('Bins')
     plt.ylabel('Count')
     plt.legend(loc="upper right")
-    #plt.savefig('image_name.png')
+    # plt.savefig('image_name.png')
     plt.show()
 
 
 if __name__ == "__main__":
     alpha, beta = 1, 2  # location and scale
     n = [100, 1000, 10000]  # add 10000
-    max_iters = 1000 # Maximum number of iterations
+    max_iters = 1000  # Maximum number of iterations
     gema = 0.001
-    # create 10 set for every n and randomly create gumbel distribution
-    dataset = [np.random.gumbel(alpha, beta, j) for i in range(10) for j in n]
-    # for loop for every data in our dataset
-    for data in dataset:
-        # estimating parameters
-        b, m = estimating_parameters(data, max_iters, gema)
 
-        # mean and std with respect to our parameters
-        mean = m + (0.57721 * beta)
-        std = (beta * np.pi) / (np.sqrt(6))
-        print("Mean : {0}  Std : {1} α : {2}  β : {3}".format(mean, std, m, b))
-
-    visualization(dataset[1], beta, alpha, 100)
-    visualization(dataset[12], beta, alpha, 1000)
-    visualization(dataset[23], beta, alpha, 10000)
+    for value in n:
+        alpha_list = list()
+        beta_list = list()
+        mean_list = list()
+        std_list = list()
+        # create 10 set for every n and randomly create gumbel distribution
+        dataset = [np.random.gumbel(alpha, beta, value) for j in range(10)]
+        # for loop for every data in our dataset
+        for data in dataset:
+            # estimating parameters
+            b, m = estimating_parameters(data, max_iters, gema)
+            mean = m + (0.57721 * beta)
+            std = (beta * np.pi) / (np.sqrt(6))
+            alpha_list.append(m)
+            beta_list.append(b)
+            mean_list.append(mean)
+            std_list.append(std)
+        df = pd.DataFrame({"Alpha": alpha_list, "Beta": beta_list, "Mean_signle_Data":mean_list, "std_signle_Data":std_list})
+        df = df.T
+        print("Table for {}".format(value))
+        print(df)
+        visualization(dataset[1], beta, alpha, value)
